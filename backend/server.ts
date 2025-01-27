743811dedf38a3c4e7e3af32ea0c767628968be5
Alan <zeyu.weng@gmail.com>
2025-01-27 02:19:24 -0500

    able to fetch emojis from server on render

--- backend/server.ts
+++ backend/server.ts
@@ -8,20 +8,28 @@ app.use(cors())
 app.use(express.json())
 const port = 3001
 
-let emojiReactions = {}
+interface EmojiReaction {
+	timestamp: number
+	userId: string
+	emoji: string
+	position: number
+}
+
+let emojiReactions: Record<string, EmojiReaction[]> = {}
 
 app.post("/addReaction", (req, res) => {
 	const { timestamp, emoji, position, userId } = req.body
 	if (!emojiReactions[timestamp]) {
 		emojiReactions[timestamp] = []
 	}
-	emojiReactions[timestamp].push({ userId, emoji, position })
+	emojiReactions[timestamp].push({ userId, emoji, position, timestamp })
 	console.log(emojiReactions)
 	res.status(200).send("Reaction added")
 })
 
 app.get("/getReactions", (req, res) => {
-	res.json(emojiReactions)
+	const formattedResponse = Object.values(emojiReactions).flat()
+	res.json(formattedResponse)
 })
 
 app.listen(port, () => {
